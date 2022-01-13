ccb08e46003fd5302d37fc198c7c2da3ba778805
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-13 12:48:33 +0530

    Execution code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Employee_Wages_usingObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_EmpCheck Uc1 = new UC1_EmpCheck();
+            UC1_EmpCheck.CheckEmployee();
+        //    UC2_DailyWage Uc2 = new UC2_DailyWage();
+        //    UC2_DailyWage.CheckDailySalary();
+        //    UC3_PartTimeEmpAndWage Uc3 = new UC3_PartTimeEmpAndWage();
+         //   UC3_PartTimeEmpAndWage.CheckPartTimeEmpWage();
+        //    UC4_PartTimeEmpWage_usingSwitch Uc4 = new UC4_PartTimeEmpWage_usingSwitch();
+        //    UC4_PartTimeEmpWage_usingSwitch.CheckEmpWageUsingSwitch();
+        //    UC5_CalculateEmpSalaryForMonth Uc5 = new UC5_CalculateEmpSalaryForMonth();
+        //    UC5_CalculateEmpSalaryForMonth.CheckFor20DaysInMonth();
+        //    UC6_SalaryForTotalWorkingHrs100_OrMonthsOf20 Uc6 = new UC6_SalaryForTotalWorkingHrs100_OrMonthsOf20();
+         //   UC6_SalaryForTotalWorkingHrs100_OrMonthsOf20.CheckUC6_SalaryForTotalWorkingHrs100_OrMonthsOf20();
+            {
+                 
+            };
+        }
+    }
+}
